d9f1a97d9d07c96cec338c305c280ac0a359b986
Maria Kushlianskaya <mariakazakova92@gmail.com>
2019-03-06 22:26:20 +0300

    continue with array methods

---
+++ 02 - arrays/task5.js
@@ -0,0 +1,8 @@
+var arr = [5, 2, 1, -10, 8];
+arr.sort().reverse();
+alert( arr );
+
+
+
+
+
---
+++ 02 - arrays/task6.js
@@ -0,0 +1,4 @@
+var arr = ["HTML", "JavaScript", "CSS"];
+var arrSorted = arr.slice().sort();
+alert( arrSorted ); // CSS, HTML, JavaScript
+alert( arr ); // HTML, JavaScript, CSS (без изменений)
